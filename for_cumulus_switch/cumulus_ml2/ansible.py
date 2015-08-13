91f1694e2f30c970a9bfe666d2537dbc40c42980
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-08-13 14:35:07 +0000

    add test to delete a bridge

--- for_cumulus_switch/cumulus_ml2/ansible.py
+++ for_cumulus_switch/cumulus_ml2/ansible.py
@@ -1,9 +1,20 @@
 def create_bridge(bridgename):
-  """creates a bridge interface using cumulus ansible module
-  Args:
-    bridgename(str): name of the bridge to create
-  Returns:
-    None: if successful
-    An error message string if failed
-  """
-  pass
+    """creates a bridge interface using cumulus ansible module
+    Args:
+        bridgename(str): name of the bridge to create
+    Returns:
+        None: if successful
+        An error message string if failed
+    """
+    pass
+
+
+def delete_bridge(bridgename):
+    """deletes a bridge interface using cumulus ansible module
+    Args:
+        bridgename(str): name of the bridge to create
+    Returns:
+        None: if successful
+        An error message string if failed
+    """
+    pass
--- for_cumulus_switch/tests/test_cumulus_ml2/test_api.py
+++ for_cumulus_switch/tests/test_cumulus_ml2/test_api.py
@@ -26,3 +26,22 @@ class TestApi(object):
         mock_using_ansible.return_value = None
         response = self.app.put('/networks/%s' % (network_id))
         assert_equals(response.status_code, 200)
+
+    @mock.patch('cumulus_ml2.ansible.delete_bridge')
+    @mock.patch('cumulus_ml2.api.send_400_fail')
+    @mock.patch('cumulus_ml2.api.send_200_ok')
+    def test_delete_bridge(self,
+                           mock_200_ok, mock_400_fail,
+                           mock_using_ansible):
+        network_id = '1111222333323434'
+        mock_400_fail.return_value = Response(status=400)
+        mock_200_ok.return_value = Response(status=200)
+        # if ansible generates an error message
+        mock_using_ansible.return_value = 'error_msg'
+        response = self.app.delete('/networks/%s' % (network_id))
+        assert_equals(response.status_code, 400)
+        mock_using_ansible.assert_called_with('brq11112223333')
+        # if ansible generates no errors
+        mock_using_ansible.return_value = None
+        response = self.app.delete('/networks/%s' % (network_id))
+        assert_equals(response.status_code, 200)
