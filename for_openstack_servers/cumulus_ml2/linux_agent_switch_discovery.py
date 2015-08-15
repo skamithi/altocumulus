426b2db6534a1a0a72a46106355f9f7cf3aad6cc
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-08-15 02:14:20 +0000

    interface_mappings is required to get the switch discovery agent to work

--- for_openstack_servers/cumulus_ml2/linux_agent_switch_discovery.py
+++ for_openstack_servers/cumulus_ml2/linux_agent_switch_discovery.py
@@ -28,7 +28,7 @@ import sys
 
 from oslo_config import cfg
 from oslo_log import log as logging
-
+from neutron.common import utils as neutron_utils
 from neutron.common import config as common_config
 from neutron.i18n import _LI
 from neutron.plugins.linuxbridge.agent import linuxbridge_neutron_agent as lna
@@ -38,17 +38,18 @@ LOG = logging.getLogger(__name__)
 
 
 CONFIG = [
-    cfg.StrOpt('switches', default=[],
-               help=_('list of switch name/ip and remote switch port connected to this compute node'))
+    cfg.ListOpt('switches', default=[],
+                help=_('list of switch name/ip and remote switch port connected to this compute node'))
 ]
 
 cfg.CONF.register_opts(CONFIG, "linux_agent_switch_discovery")
 
 
 class LinuxBridgeSwitchDiscoveryNeutronAgentRPC(lna.LinuxBridgeNeutronAgentRPC):
-    def __init__(self, polling_interval):
+    def __init__(self, interface_mapping, polling_interval):
         """ read the ml2 config and add each remote switch details to the neutron DB"""
         super(LinuxBridgeSwitchDiscoveryNeutronAgentRPC, self).__init__(
+            interface_mapping,
             polling_interval
         )
 
@@ -70,7 +71,10 @@ def main():
     common_config.init(sys.argv[1:])
     common_config.setup_logging()
     polling_interval = cfg.CONF.AGENT.polling_interval
-    agent = LinuxBridgeSwitchDiscoveryNeutronAgentRPC(polling_interval)
+    interface_mappings = neutron_utils.parse_mappings(
+        cfg.CONF.LINUX_BRIDGE.physical_interface_mappings)
+    agent = LinuxBridgeSwitchDiscoveryNeutronAgentRPC(interface_mappings,
+                                                      polling_interval)
     LOG.info(_LI("Agent initialized successfully, now running... "))
     agent.daemon_loop()
     sys.exit(0)
