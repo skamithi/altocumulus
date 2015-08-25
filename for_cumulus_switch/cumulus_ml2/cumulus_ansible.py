71c6ac08b112322a7f239bccb8c913c060a2fd18
stanley karunditu <stanleyk@cumulusnetworks.com>
2015-08-25 16:32:01 -0400

    add tests for cumulus_ansible.updatE_bridge_vlan_list and update_port_vlan_list

--- for_cumulus_switch/cumulus_ml2/cumulus_ansible.py
+++ for_cumulus_switch/cumulus_ml2/cumulus_ansible.py
@@ -1,17 +1,17 @@
 from netshowlib import netshowlib
-import ansible.runner
-import pkg_resources
-import os
+from netshowlib.linux.common import create_range
+
 
 def get_vlan_aware_bridge():
     portlist = netshowlib.portname_list()
     for _portname in portlist:
         _iface = netshowlib.iface(_portname)
-        if hasattr(iface, 'vlan_filtering') and \
+        if hasattr(_iface, 'vlan_filtering') and \
                 _iface.vlan_filtering() and \
                 _iface.is_bridge():
                     return _iface
     return None
+
 
 class CumulusML2Ansible(object):
     def __init__(self, bridgename, vlan_id, port_id):
@@ -20,14 +20,28 @@ class CumulusML2Ansible(object):
         self.vlan = vlan_id
         self.vlan_aware_bridge = get_vlan_aware_bridge()
 
-    def update_bridge_vlan_aware(self):
-        pass
+    def update_port_vlan_list(self):
+        """ removes or adds vlans to the bridge vlan list.
+        """
+        port_vlan_list = netshowlib.iface(self.port).vlan_list
+        port_vlan_list.append(self.vlan)
+        return create_range('', set(port_vlan_list))
+
+    def update_bridge_vlan_list(self):
+        """ get a list of all vlans found on the vlan aware bridge
+        """
+        vlan_list = []
+        bridgemems = self.vlan_aware_bridge.members.values()
+        for _member in bridgemems:
+            vlan_list += (_member.vlan_list)
+
+        return create_range('', set(vlan_list))
 
     def update_bridge_classic_mode(self):
         pass
 
     def add_to_bridge(self):
         if self.in_vlan_aware_mode():
-            return self.update_bridge_vlan_aware()
+            return self.add_to_bridge_vlan_aware()
         else:
-            return self.update_bridge_classic_mode()
+            return self.add_to_bridge_classic_mode()
--- for_cumulus_switch/tests/test_cumulus_ml2/test_cumulus_ansible.py
+++ for_cumulus_switch/tests/test_cumulus_ml2/test_cumulus_ansible.py
@@ -1,23 +1,42 @@
-from nose.tools import assert_equals
+from asserts import assert_equals
 from cumulus_ml2 import cumulus_ansible
 import mock
 
+
 class TestCumulusML2Ansible(object):
 
-  @mock.patch('cumulus_ml2.cumulus_ansible.get_vlan_aware_bridge')
-  def setup(self, mock_vlan_aware):
-    mock_vlan_aware.return_value = None
-    self.myobject = cumulus_ansible.CumulusML2Ansible(bridgename='brq11111',
-        vlan_id='123', port_id='bond11')
+    @mock.patch('cumulus_ml2.cumulus_ansible.get_vlan_aware_bridge')
+    def setup(self, mock_vlan_aware):
+        mock_vlan_aware.return_value = None
+        self.myobject = cumulus_ansible.CumulusML2Ansible(bridgename='brq11111',
+                                                          vlan_id='123', port_id='bond11')
 
-  def test_add_to_bridge_vlan_aware_works(self):
-    self.myobject.in_vlan_aware_mode = mock.MagicMock(return_value=True)
-    self.myobject.update_bridge_vlan_aware = mock.MagicMock(
-        return_value='exec bridge vlan aware func')
-    assert_equals(self.myobject.add_to_bridge(), 'exec bridge vlan aware func')
+    def test_add_to_bridge_vlan_aware_works(self):
+        self.myobject.in_vlan_aware_mode = mock.MagicMock(return_value=True)
+        self.myobject.add_to_bridge_vlan_aware = mock.MagicMock(
+            return_value='exec bridge vlan aware func')
+        assert_equals(self.myobject.add_to_bridge(), 'exec bridge vlan aware func')
 
-  def test_add_to_bridge_classic_mode(self):
-    self.myobject.in_vlan_aware_mode = mock.MagicMock(return_value=False)
-    self.myobject.update_bridge_classic_mode = mock.MagicMock(
-        return_value='exec bridge vlan classic mode')
-    assert_equals(self.myobject.add_to_bridge(), 'exec bridge vlan classic mode')
+    def test_add_to_bridge_classic_mode(self):
+        self.myobject.in_vlan_aware_mode = mock.MagicMock(return_value=False)
+        self.myobject.add_to_bridge_classic_mode = mock.MagicMock(
+            return_value='exec bridge vlan classic mode')
+        assert_equals(self.myobject.add_to_bridge(), 'exec bridge vlan classic mode')
+
+    @mock.patch('cumulus_ml2.cumulus_ansible.netshowlib.iface')
+    def test_update_port_vlan_list(self, mock_iface):
+        instance = mock_iface.return_value
+        instance.vlan_list = ['1', '2', '3']
+        self.myobject.vlan = '4'
+        assert_equals(self.myobject.update_port_vlan_list(), ['1-4'])
+
+    @mock.patch('cumulus_ml2.cumulus_ansible.netshowlib.iface')
+    def test_update_bridge_vlan_list(self, mock_iface):
+        main_bridge = mock_iface.return_value
+        member1 = mock.MagicMock()
+        member1.vlan_list = ['1', '2']
+        member2 = mock.MagicMock()
+        member2.vlan_list = ['3', '4']
+        main_bridge.members = {'member1': member1, 'member2': member2}
+        self.myobject.vlan_aware_bridge = main_bridge
+        assert_equals(self.myobject.update_bridge_vlan_list(), ['1-4'])
